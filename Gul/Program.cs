5186b25c62869a9b22b34c89d231a5b4afabb5b8
TheNerdySlayer <lasse-g@energimail.dk>
2024-09-23 11:02:33 +0200

    Add project files.

---
+++ Gul/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gul
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter anything... ");
+            Console.WriteLine(Console.ReadLine());
+            Console.WriteLine("What do you mean?...");
+            Console.Write("What:");
+        }
+    }
+}
---
+++ Rød/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Rød
+{
+    internal class Program
+    {
+        protected enum Tone
+        {
+            REST = 0,
+            GbelowC = 196,
+            A = 220,
+            Asharp = 233,
+            B = 247,
+            C = 262,
+            Csharp = 277,
+            D = 294,
+            Dsharp = 311,
+            E = 330,
+            F = 349,
+            Fsharp = 370,
+            G = 392,
+            Gsharp = 415,
+        }
+        protected enum Duration
+        {
+            WHOLE = 1600,
+            HALF = WHOLE / 2,
+            QUARTER = HALF / 2,
+            EIGHTH = QUARTER / 2,
+            SIXTEENTH = EIGHTH / 2,
+        }
+        protected static void Play(Note[] tune)
+        {
+            foreach (Note n in tune)
+            {
+                if (n.NoteTone == Tone.REST)
+                    Thread.Sleep((int)n.NoteDuration);
+                else
+                    Console.Beep((int)n.NoteTone, (int)n.NoteDuration);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.BackgroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("Is this even readable?: ");
+            Console.Beep(277, 1000);
+            Note[] Mary =
+        {
+        new Note(Tone.B, Duration.QUARTER),
+        new Note(Tone.A, Duration.QUARTER),
+        new Note(Tone.GbelowC, Duration.QUARTER),
+        new Note(Tone.A, Duration.QUARTER),
+        new Note(Tone.B, Duration.QUARTER),
+        new Note(Tone.B, Duration.QUARTER),
+        new Note(Tone.B, Duration.HALF),
+        new Note(Tone.A, Duration.QUARTER),
+        new Note(Tone.A, Duration.QUARTER),
+        new Note(Tone.A, Duration.HALF),
+        new Note(Tone.B, Duration.QUARTER),
+        new Note(Tone.D, Duration.QUARTER),
+        new Note(Tone.D, Duration.HALF)
+        };
+            Play(Mary);
+            Console.ReadKey();
+        }
+        protected struct Note
+        {
+            Tone toneVal;
+            Duration durVal;
+
+            // Define a constructor to create a specific note.
+            public Note(Tone frequency, Duration time)
+            {
+                toneVal = frequency;
+                durVal = time;
+            }
+
+            // Define properties to return the note's tone and duration.
+            public Tone NoteTone { get { return toneVal; } }
+            public Duration NoteDuration { get { return durVal; } }
+        }
+    }
+}
